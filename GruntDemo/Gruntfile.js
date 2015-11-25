d56354b4ec939de002bad25fb01908d5dc1459d6
yanjj <yanjj@Ctrip.com>
2015-11-25 10:33:07 +0800

    Add md5 grunt demo

--- GruntDemo/Gruntfile.js
+++ GruntDemo/Gruntfile.js
@@ -1,29 +1,50 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		rev: {
-			options: {
-				encoding: 'utf8',
-				algorithm: 'md5',
-				length: 8
-			},
-			assets: {
-				files: [{
-					src: ['dest/text1.txt']
-				}]
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-rev');
-	// grunt.registerTask('default', 'rev');
+    grunt.initConfig({
+        rev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 8
+            },
+            assets: {
+                files: [{
+                    src: ['dest/text1.txt']
+                }]
+            }
+        },
+        md5: {
+            compile: {
+                files: {
+                    'dest/md5/': 'src/**/*'
+                },
+                options: {
+                    encoding: null,
+                    keepBasename: true,
+                    keepExtension: true,
+                    afterEach: function(fileChange, options) {
+                    	console.log(fileChange);
+                    },
+                    after: function(fileChanges, options) {
+                        
+                    }
+                }
+            }
+        }
+    });
 
-	grunt.registerTask('md5', function() {
-		var fs = require('fs');
-		var md5 = require('md5');
-		console.log(md5('message'));
 
-		var data = fs.readFileSync('index.html');
-		console.log(md5(data));
-	});
+    grunt.loadNpmTasks('grunt-rev');
+    grunt.loadNpmTasks('grunt-md5');
+    grunt.registerTask('default', 'md5');
 
-	grunt.registerTask('default', 'md5');
+/*    grunt.registerTask('md5', function() {
+        var fs = require('fs');
+        var md5 = require('md5');
+        console.log(md5('message'));
+
+        var data = fs.readFileSync('index.html');
+        console.log(md5(data));
+    });
+
+    grunt.registerTask('default', 'md5');*/
 };
---
+++ GruntDemo/dest/md5/Test-cef130c91f2d3442dc62ee4e239cc97f.js
@@ -0,0 +1,7 @@
+define(['test1','test2'],function(test1,test2){
+    var exports={
+    	test1:test1.test1,
+        test2:test2.test2
+    };
+    return exports;
+});
