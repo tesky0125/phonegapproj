ca057151eb8f29bc288c172e2c79ff52c13e03e3
RylanYan <rylan_yan@qq.com>
2016-02-28 20:55:33 +0800

    Add custrom-ctrl folder and add imageswiper.

--- custom-ctrl/iselecter-react/main_iselect_react.js
+++ custom-ctrl/iselecter-react/main_iselect_react.js
@@ -20,20 +20,3 @@ ReactDom.render(
 	<ISelecter container="#selecter" options={options} onselectchange={onselectchange}/>,
 	document.getElementById("selecter")
 );
-
-// $(function(){
-// 	// var style = window.getComputedStyle(document.getElementById('selecter'));
-// 	// var width = parseInt(style.width.replace(/^(\d+).*/,'$1'),10);
-// 	var data = ['100元', '200元', '300元', '400元', '500元', '600元'];
-// 	var iSelecter = new ISelecter('#selecter',{
-// 		width:500/*width*/,//unit:px
-// 		height:100,
-// 		displayNum:5,
-// 		data:data,
-// 		index:2
-// 	});
-// 	iSelecter.onselectchange = function(index, data){
-// 		console.log('index:'+index);
-// 		console.log('data:'+data);
-// 	}
-// });
