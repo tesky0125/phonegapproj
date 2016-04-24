d7f26f6ee76643cfa146e823a7046fad8ae523d4
RylanYan <rylan_yan@qq.com>
2016-04-24 21:16:04 +0800

    Update comment for inherit.js

--- inherit.js
+++ inherit.js
@@ -9,10 +9,10 @@ Parent.prototype.speak=function(){
 }
 
 function Man(name,age){
-	//属性继承
+	//继承属性(属性借用)
 	Parent.call(this,'male',name,age)
 }
-//方法继承(原型继承)
+//继承方法(原型继承)
 //1.Man.prototype = Parent.prototype;
 //缺点：修改Man.prototype,会影响Parent.prototype
 //2.Man.prototype = new Parent();
@@ -23,20 +23,25 @@ ctor.prototype = Parent.prototype;
 Man.prototype = new ctor();
 //还原constructor
 Man.prototype.constructor = Man;
+//设置父类
 Man.__super__ = Parent.prototype;
 
 
 //原型继承函数实现
 __extends = function(child, parent) { 
+	//继承属性(属性借用)
 	for (var key in parent) {
 		if (__hasProp.call(parent, key)) child[key] = parent[key]; 
 	} 
 
+	//继承方法
 	function ctor() { 
+		//还原constructor
 		this.constructor = child; 
 	} 
 	ctor.prototype = parent.prototype; 
 	child.prototype = new ctor(); 
+	//设置父类
 	child.__super__ = parent.prototype; 
 	return child; 
 };
