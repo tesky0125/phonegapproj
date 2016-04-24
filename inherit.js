4dde3de999ec8cc5f072a6a7a961cabf3d9b894c
RylanYan <rylan_yan@qq.com>
2016-04-24 22:05:48 +0800

    Update advanced function inherit.js

--- inherit.js
+++ inherit.js
@@ -1,47 +1,73 @@
 //原型继承原理分析
-function Parent(sex,name,age){
-	this.sex;
-	this.name;
-	this.age;
+function Parent(sex, name, age) {
+    //成员变量
+    this.sex;
+    this.name;
+    this.age;
 }
-Parent.prototype.speak=function(){
-	console.log('this is',name,sex,age)
-}
+//成员方法
+Parent.prototype.speak = function() {
+        console.log('this is', name, sex, age)
+    }
+    //静态成员与方法
+Parent.staticMember;
+Parent.staticMethod = function() {}
 
-function Man(name,age){
-	//继承属性(属性借用)
-	Parent.call(this,'male',name,age)
+
+function Child(name, age) {
+    //继承成员变量(属性借用)
+    Parent.call(this, 'male', name, age)
 }
-//继承方法(原型继承)
-//1.Man.prototype = Parent.prototype;
-//缺点：修改Man.prototype,会影响Parent.prototype
-//2.Man.prototype = new Parent();
+//继承静态成员与方法
+_.extend(Child, Parent);
+
+//继承成员方法(原型继承)
+//1.Child.prototype = Parent.prototype;
+//缺点：修改Child.prototype,会影响Parent.prototype
+//2.Child.prototype = new Parent();
 //缺点：无效会执行Parent的构造函数
 //3.
-function ctor(){}
+function ctor() {}
 ctor.prototype = Parent.prototype;
-Man.prototype = new ctor();
+Child.prototype = new ctor();
 //还原constructor
-Man.prototype.constructor = Man;
+Child.prototype.constructor = Child;
 //设置父类
-Man.__super__ = Parent.prototype;
+Child.__super__ = Parent.prototype;
+
 
 
 //原型继承函数实现
-__extends = function(child, parent) { 
-	//继承属性(属性借用)
-	for (var key in parent) {
-		if (__hasProp.call(parent, key)) child[key] = parent[key]; 
-	} 
+var inherits = function(parent, protoProps, staticProps) {
+    var child;
 
-	//继承方法
-	function ctor() { 
-		//还原constructor
-		this.constructor = child; 
-	} 
-	ctor.prototype = parent.prototype; 
-	child.prototype = new ctor(); 
-	//设置父类
-	child.__super__ = parent.prototype; 
-	return child; 
+    //继承属性(属性借用)
+    child = function() {
+        parent.apply(this, arguments);
+    };
+
+    //继承静态成员与方法
+    _.extend(child, parent);
+
+    //继承方法
+    function ctor() {}
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor();
+
+    //扩展成员属性
+    if (protoProps)
+        _.extend(child.prototype, protoProps);
+
+    //扩展静态属性
+    if (staticProps)
+        _.extend(child, staticProps);
+
+    //还原constructor
+    child.prototype.constructor = child;
+
+    //设置父类
+    //子类.__super__.constructor.call(this);
+    child.__super__ = parent.prototype;
+
+    return child;
 };
